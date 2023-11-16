d1aebb34f1c60456dd1de5f7d3da449ea0e0fb63
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2023-11-16 10:46:12 +0700

    feat(app): add rate limiter

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -7,6 +7,8 @@ import { TypeormConfigService } from '../database/typeorm-config.service';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import authConfig from './auth.config';
 import redisConfig from './redis.config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -23,7 +25,30 @@ import redisConfig from './redis.config';
         return new DataSource(options).initialize();
       },
     }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
-  exports: [NestConfigModule, TypeOrmModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+  exports: [NestConfigModule, TypeOrmModule, ThrottlerModule],
 })
 export class ConfigModule {}
