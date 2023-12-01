64f99c684e3ceecf6df066f27ef14c56daa9ab9a
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2023-12-01 12:15:45 +0700

    swagger(user): update swagger schema and properties in get user data API

--- src/users/decorators/functions/swagger/me.decorator.ts
+++ src/users/decorators/functions/swagger/me.decorator.ts
@@ -12,18 +12,51 @@ export function MeSwaggerDecorator() {
     ApiBearerAuth(),
     ApiOkResponse({
       schema: {
+        type: 'object',
+        properties: {
+          id: { type: 'string', format: 'uuid' },
+          username: { type: 'string' },
+          email: {
+            oneOf: [{ type: 'string', format: 'email' }, { type: 'null' }],
+          },
+          tasks: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                id: { type: 'string', format: 'uuid' },
+                title: { type: 'string' },
+                description: { type: 'string' },
+                status: {
+                  type: 'string',
+                  enum: ['OPEN', 'IN_PROGRESS', 'DONE'],
+                },
+              },
+            },
+          },
+        },
         example: {
           id: 'dc4f688d-6c65-4aa3-a1aa-4ea80c59b199',
           username: 'user_name',
           email: null,
-          createdAt: '2023-11-15T22:12:41.445Z',
-          updatedAt: '2023-11-15T22:12:41.445Z',
-          deletedAt: null,
+          tasks: [
+            {
+              id: '09818032-c0d8-4466-9c5c-c58788e90dec',
+              title: 'Go to Market',
+              description: 'Buy some vegetables and milk',
+              status: 'DONE',
+            },
+          ],
         },
       },
     }),
     ApiUnauthorizedResponse({
       schema: {
+        type: 'object',
+        properties: {
+          message: { type: 'string' },
+          statusCode: { type: 'number' },
+        },
         example: {
           message: 'Unauthorized',
           statusCode: 401,
